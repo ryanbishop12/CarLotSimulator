0c151af141fc738b5f01ef303e0625a9178f06ea
thatpersoninthemirror <ryan.bishop258@gmail.com>
2020-01-23 13:24:15 -0600

    finished assignment

---
+++ CarLotSimulatorApp/CarLotSimulator/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    class Car
+    {
+
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public string HonkNoise { get; set; }
+        public bool IsDriveable { get; set; }
+
+
+        //TODO
+        public Car()
+        {
+
+        }
+        public Car(int y, string mak, string mod, string en, string hn, bool drive)
+        {
+            Year = y;
+            Make = mak;
+            Model = mod;
+            EngineNoise = en;
+            HonkNoise = hn;
+            IsDriveable = drive;
+        }
+
+        //Create a seperate class file called Car
+        //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+        //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
+        //The methods should take one string parameter: the respective noise property
+
+        public void MakeEngineNoise(string noise)
+        {
+            Console.WriteLine(noise);
+        }
+        public void MakeHonkNoise(string honk)
+        {
+            Console.WriteLine(honk);
+        }
+    }
+}
---
+++ CarLotSimulatorApp/CarLotSimulator/CarLot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarLotSimulator
+{
+    class CarLot
+    {
+        public List<Car> Cars { get; set; } = new List<Car>();
+
+        public CarLot()
+        {
+            
+        }
+        public CarLot(List<Car> input)
+        {
+            Cars = input;
+        }
+    }
+}
